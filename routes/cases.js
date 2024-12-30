0915d5bbb98a1d4115570e62e0c9667ff3ccb0f2
princiliz <ardechedemanou@gmail.com>
2024-12-30 16:56:02 +0100

    modifications

--- routes/cases.js
+++ routes/cases.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const caseController = require('../controllers/caseController');
 
 router.get('/', caseController.getAllCases);
-router.post('/', caseController.generateCase);
+router.post('/generate', caseController.generateCase);
 router.delete('/:id', caseController.deleteCase);
 
 module.exports = router;
